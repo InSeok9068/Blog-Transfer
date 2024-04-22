9e0de2f18c76a39dc0521af41d728499a486d842
Lee In Seok <dlstjr9068@gmail.com>
2024-04-22 15:16:13 +0900

    컴포넌트 처리

--- astro.config.js
+++ astro.config.js
@@ -2,7 +2,6 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vue from '@astrojs/vue';
 import sitemap from '@astrojs/sitemap';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
